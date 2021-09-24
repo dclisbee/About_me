f999d9505d2b468f006cd39d8a96594dd1a3a5db
dclisbee <dmclisbee@gmail.com>
2021-09-24 18:31:50 -0500

    push

---
+++ CSS - My Site/app.js
@@ -0,0 +1,14 @@
+const skills = document.querySelector(".skills");
+const projects = document.querySelector(".projects");
+const profile = document.querySelector(".profile");
+const top = document.querySelector(".top-div");
+const bot = document.querySelector(".bot-div");
+
+
+
+const botdiv = document.querySelector('.contact-s');
+botdiv.addEventListener('click', () => {
+    botdiv.scrollIntoView({
+        behavior: 'smooth'
+    })
+})
